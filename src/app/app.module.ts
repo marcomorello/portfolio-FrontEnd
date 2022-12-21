c386bd8af9a0889ca736a2031d9ba62249f04407
Marco <marcomorello99@gmail.com>
2022-12-21 19:03:01 -0300

    Rediseño del sitio en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,52 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { BannerComponent } from './banner/banner.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { EstudioComponent } from './estudio/estudio.component';
+import { SkillsComponent } from './skills/skills.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    BannerComponent,
+    ExperienciaComponent,
+    EstudioComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgCircleProgressModule.forRoot({
+      "radius": 60,
+      "space": -10,
+      "outerStrokeGradient": true,
+      "outerStrokeWidth": 10,
+      "outerStrokeColor": "#4882c2",
+      "outerStrokeGradientStopColor": "#53a9ff",
+      "innerStrokeColor": "#e7e8ea",
+      "innerStrokeWidth": 10,
+      "title": "UI",
+      "animateTitle": false,
+      "animationDuration": 1000,
+      "showUnits": false,
+      "showBackground": false,
+      "clockwise": true,
+      "startFromZero": false,
+      "lazy": false,
+    })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
