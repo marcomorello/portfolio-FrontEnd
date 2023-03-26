d2ca59ed0ab98b6d04e33602938fef4b6e715179
marcomorello <97745191+marcomorello@users.noreply.github.com>
2023-03-25 23:27:21 -0300

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AcercaDeComponent } from './acerca-de/acerca-de.component';
@@ -11,7 +10,12 @@ import { SkillsComponent } from './skills/skills.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { ProyectosComponent } from './proyectos/proyectos.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AppRountingModule } from './app-rounting.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,32 +26,38 @@ import { FooterComponent } from './footer/footer.component';
     EstudioComponent,
     SkillsComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     NgCircleProgressModule.forRoot({
-      "radius": 60,
-      "space": -10,
-      "outerStrokeGradient": true,
-      "outerStrokeWidth": 10,
-      "outerStrokeColor": "#4882c2",
-      "outerStrokeGradientStopColor": "#53a9ff",
-      "innerStrokeColor": "#e7e8ea",
-      "innerStrokeWidth": 10,
-      "title": "UI",
-      "animateTitle": false,
-      "animationDuration": 1000,
-      "showUnits": false,
-      "showBackground": false,
-      "clockwise": true,
-      "startFromZero": false,
-      "lazy": false,
-    })
-
+      radius: 60,
+      space: -10,
+      outerStrokeGradient: true,
+      outerStrokeWidth: 10,
+      outerStrokeColor: '#4882c2',
+      outerStrokeGradientStopColor: '#53a9ff',
+      innerStrokeColor: '#e7e8ea',
+      innerStrokeWidth: 10,
+      title: 'UI',
+      animateTitle: false,
+      animationDuration: 1000,
+      showUnits: false,
+      showBackground: false,
+      clockwise: true,
+      startFromZero: false,
+      lazy: false,
+    }),
+    HttpClientModule,
+    AppRountingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    interceptorProvider
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-
+export class AppModule {}
